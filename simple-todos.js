e045f1abd4fcf2c010f65c7e1d23839836b9c1db
smclaren727 <smclaren727@gmail.com>
2015-10-25 20:16:25 -0400

    Added complete and delete todo CRUD

--- simple-todos.js
+++ simple-todos.js
@@ -25,5 +25,17 @@ if (Meteor.isClient) {
       // Clear form
       event.target.text.value = "";
     }
-  })
+  });
+
+  Template.task.events({
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current value
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete": function () {
+      Tasks.remove(this._id);
+    }
+  });
 }
